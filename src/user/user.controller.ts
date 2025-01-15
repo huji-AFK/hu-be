0279b9df5172246e9cc28db6f66a53010abbd4b5
彪 <3439436471@qq.com>
2025-01-15 18:45:44 +0800

    去除渲染

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Post, Body, Response, Render } from '@nestjs/common';
+import { Controller, Get, Post, Body, Response } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   @Get()
-  @Render('default/user.ejs')
+
   index() {
   }
 
